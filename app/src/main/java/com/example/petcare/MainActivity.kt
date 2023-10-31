85afa0503f7865cd570eb88d6ecc8ec0af0495e0
Joel <joelgarciag04@gmail.com>
2023-10-31 19:06:40 +0100

    Bottom navegation funcional + image asset

--- app/src/main/java/com/example/petcare/MainActivity.kt
+++ app/src/main/java/com/example/petcare/MainActivity.kt
@@ -9,10 +9,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.petcare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val bottomNavigationView = binding.bottomNavigation
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
