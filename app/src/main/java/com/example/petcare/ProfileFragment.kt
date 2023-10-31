da80b2d0c0c25f73160606b3431a767499e139fe
Joel <joelgarciag04@gmail.com>
2023-10-31 18:31:10 +0100

    navegación entre fragments

--- app/src/main/java/com/example/petcare/ProfileFragment.kt
+++ app/src/main/java/com/example/petcare/ProfileFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.petcare.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
+    lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        savedInstanceState: Bundle?): View? {
+        binding= FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.editButton.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_editProfileFragment)
+        }
     }
 
 }
