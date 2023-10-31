4915f498c72e3b09f8392bf397f53492656cf047
roman <muhammad-roman.aziz.7e5@itb.cat>
2023-10-31 16:40:04 +0100

    Menú visible

--- app/src/main/java/com/example/petcare/MainActivity.kt
+++ app/src/main/java/com/example/petcare/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         val bottomNavigationView = binding.bottomNavigation
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
