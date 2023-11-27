e98db793b483a61c8bddc075a5f2d2b22b12ea36
Joel <joelgarciag04@gmail.com>
2023-11-27 16:29:07 +0100

    menu desplegable

--- app/src/main/java/com/example/petcare/view/MenuFragment.kt
+++ app/src/main/java/com/example/petcare/view/MenuFragment.kt
@@ -6,15 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.petcare.R
+import com.example.petcare.databinding.FragmentMenuBinding
 
+lateinit var binding: FragmentMenuBinding
 class MenuFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        savedInstanceState: Bundle?): View? {
+       binding= FragmentMenuBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.myservice.setOnClickListener{
+            if (binding.text1.visibility == View.GONE){
+                binding.text1.visibility=View.VISIBLE
+            }
+            else binding.text1.visibility=View.GONE
+        }
+        binding.servicedone.setOnClickListener{
+            if (binding.text2.visibility == View.GONE){
+                binding.text2.visibility=View.VISIBLE
+            }
+            else binding.text2.visibility=View.GONE
+        }
+        binding.complain.setOnClickListener{
+            if (binding.text3.visibility == View.GONE){
+                binding.text3.visibility=View.VISIBLE
+            }
+            else binding.text3.visibility=View.GONE
+        }
+
     }
 
 }
