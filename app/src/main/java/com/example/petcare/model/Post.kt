9c31cde4e2040c112a685e1a6f5d091cd5134f58
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2023-12-18 16:44:49 +0100

    mejoras de publicar ofertas

--- app/src/main/java/com/example/petcare/model/Post.kt
+++ app/src/main/java/com/example/petcare/model/Post.kt
@@ -3,13 +3,12 @@ package com.example.model
 import android.os.Build
 import androidx.annotation.RequiresApi
 import kotlinx.serialization.Serializable
-import java.time.LocalDateTime
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Serializable
 data class Post(
     var postId: Int,
-    var owner: User,
+    var owner: User?,
     var reciver: User,
     var offers: List<User>,
     var tittle: String,
